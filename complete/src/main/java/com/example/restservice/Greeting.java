9ac19a428979e56aaca3fa88659856368f8e581f
gm2002909 <jhs@gabia.com>
2024-02-08 17:45:55 +0900

    v

--- complete/src/main/java/com/example/restservice/Greeting.java
+++ complete/src/main/java/com/example/restservice/Greeting.java
@@ -1,3 +1,15 @@
 package com.example.restservice;
 
-public record Greeting(long id, String content) { }
+//public record Greeting(long id, String content) { }
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    // Getter 메서드 등을 추가할 수 있음
+}
